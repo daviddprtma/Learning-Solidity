f6959319c2a5a66957d955fb69371327aea4f364
daviddprtma <davidchristianpratama@gmail.com>
2022-06-17 20:09:13 +0700

    learn solidity

---
+++ learnsolidity.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.15;
+
+contract SpaceDoggos{
+    uint maxPlanetsPerSystem = 10;
+    uint minPlanetsPerSystem = 3;
+
+    uint planetCodeDigits = 7;
+    uint systemCodeDigits = 7;
+
+    uint planetCodeModulus = 10 ** planetCodeDigits;
+    uint systemCodeModulus = 10 ** systemCodeDigits;
+
+    struct Doggo{
+      string name;
+      uint8 breed;
+      uint8 color;
+      uint8 face;
+      uint8 costume;
+      uint coordX;
+      uint coordY;
+    }
+
+    mapping(address => Doggo) doggos;
+}
